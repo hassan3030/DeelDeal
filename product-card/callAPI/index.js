e57cd5ca55e3e3c12310f9db701ea274a392cacf
Hassan Hamdi Ashour <61333986+hassan3030@users.noreply.github.com>
2025-06-17 15:05:26 +0300

    Delete product-card directory

--- product-card/callAPI/index.js
+++
@@ -1,7 +0,0 @@
-// Export all API methods
-import * as authAPI from "./auth"
-import * as productsAPI from "./products"
-import * as usersAPI from "./users"
-import api from "./config"
-
-export { authAPI, productsAPI, usersAPI, api }

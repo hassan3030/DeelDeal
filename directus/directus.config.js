ec05cb31de6095d6928874264e15ed59ceb1d619
hassan3030 <hassanhh1996eng@gmail.com>
2025-07-15 11:09:29 +0300

    gen 34th

--- directus/directus.config.js
+++ directus/directus.config.js
@@ -3,7 +3,7 @@ export default {
     providers: [
       {
         driver: 'oauth2',
-        id: 'google', 
+        id: 'google',
         name: 'Google',
         icon: 'google',
         url: 'https://accounts.google.com/o/oauth2/v2/auth',
@@ -13,17 +13,15 @@ export default {
         authorize_url: 'https://accounts.google.com/o/oauth2/v2/auth',
         response_type: 'code',
         grant_type: 'authorization_code',
-        // client_id: '602047752247-39qmihl9qoh15v24vr6ojptd5g60i9pr.apps.googleusercontent.com',
-        // client_secret: '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',
         client_id: process.env.AUTH_GOOGLE_CLIENT_ID,
         client_secret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
-        redirect_uri: process.env.AUTH_GOOGLE_REDIRECT_URI || 'http://localhost:8055/auth/login/google',
+        redirect_uri: process.env.AUTH_GOOGLE_REDIRECT_URI || `${process.env.PUBLIC_URL}/auth/login/google`,
         profile_map: {
           id: 'sub',
           email: 'email',
           first_name: 'given_name',
           last_name: 'family_name'
-        },
+        }
       }
     ]
   }
